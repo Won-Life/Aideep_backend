53f23975761a5b713643134221136b5d7be04ea6
seoki <seoki180@naver.com>
2026-03-07 21:00:16 +0900

    Etc : winston 로거 추가
    Signed-off-by: seoki <seoki180@naver.com>

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,36 @@
-import { Module } from '@nestjs/common';
+import { Logger, MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
-import { RedisModule } from './common/redis/redis.module';
+import { RedisModule } from './redis/redis.module';
 import { WorkspaceModule } from './workspace/workspace.module';
+import { SseModule } from './sse/sse.module';
 import { NodeModule } from './node/node.module';
+import * as winston from 'winston';
+import { utilities, WinstonModule } from 'nest-winston';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
+    WinstonModule.forRoot({
+      transports: [
+        new winston.transports.Console({
+          level: process.env.NODE_ENV === 'production' ? 'info' : 'silly',
+          format: winston.format.combine(
+            winston.format.timestamp(),
+            utilities.format.nestLike('AIdeep', { prettyPrint: true })
+          )
+        })
+      ]
+    }),
     PrismaModule,
     UserModule,
     AuthModule,
     RedisModule,
+    SseModule,
     WorkspaceModule,
     NodeModule
   ],
--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,15 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 import { AllExceptionsFilter } from './common/error';
 import { ResponseInterceptor } from './common/response/response.interceptor';
+import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { bufferLogs: true });
 
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
   app.useGlobalFilters(new AllExceptionsFilter());
   app.useGlobalInterceptors(new ResponseInterceptor());
+  app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
 
   const config = new DocumentBuilder()
     .setTitle('Cats example')
